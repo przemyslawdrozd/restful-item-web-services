55a3f3b71c9ec01bedffd03161fc4f20788a0c23
przemo06133 <przemo06133@gmail.com>
2020-02-14 14:18:55 +0100

    H2 connection and data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO item (id, name, price, amount) VALUES (1001, 'Banana', 1.23, 20);
+INSERT INTO item (id, name, price, amount) VALUES (1002, 'Apple', 2.23, 10);
+INSERT INTO item (id, name, price, amount) VALUES (1003, 'Milk', 1.53, 26);
+INSERT INTO item (id, name, price, amount) VALUES (1004, 'Orange', 0.23, 32);
+INSERT INTO item (id, name, price, amount) VALUES (1005, 'Water', 1.50, 2);
