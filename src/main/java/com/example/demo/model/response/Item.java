39687e68456306805893cda606dae85a5316a630
przemo06133 <przemo06133@gmail.com>
2020-02-14 15:12:10 +0100

    item model and repository

---
+++ src/main/java/com/example/demo/model/response/Item.java
@@ -0,0 +1,33 @@
+package com.example.demo.model.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @JsonIgnore
+    private Long id;
+
+    @NotNull(message = "Name is required")
+    private String name;
+    private Double price;
+    private Integer amount;
+
+    public Item(String name, Double price, Integer amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+}
