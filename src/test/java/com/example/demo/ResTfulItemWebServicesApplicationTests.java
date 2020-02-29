941dffe309d2685c5e3d2fb38e34faadff0acfdc
przemo06133 <przemo06133@gmail.com>
2020-02-29 20:55:38 +0100

    integration test

--- src/test/java/com/example/demo/ResTfulItemWebServicesApplicationTests.java
+++ src/test/java/com/example/demo/ResTfulItemWebServicesApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class ResTfulItemWebServicesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private TestRestTemplate restTemplate;
 
+	@Test
+	public void shouldGetItem() throws JSONException {
+		String response = this.restTemplate.getForObject("/items/1002", String.class);
+
+		JSONAssert.assertEquals("{name:Apple,price:2.23,amount:10}", response, false);
+	}
 }
